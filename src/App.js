8ad9deafaf60b98f5119d224701b13edc7fcd99f
vtsoma <cheesetkh39@gmail.com>
2023-06-21 13:54:54 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/nav";
+
+import './App.css'
+
+import userIcon from './assets/icons/user.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="app">
+    <div className="topbar">
+      <div className="logo">
+        <h1>wurk.</h1>
+      </div>
+      <Nav />
+      <div className="account">
+        <img src={userIcon} alt="" />
+      </div>
     </div>
+    <main>
+      <h1 className={"main-text"}>Get your wurk dun by <br/> your choice</h1>
+    </main>
+  </div>
   );
 }
 
---
+++ src/components/nav/index.js
@@ -0,0 +1,14 @@
+import './nav.module.css';
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>Main</ul>
+      <ul>Explore</ul>
+      <ul>Stories</ul>
+      <ul>BYEEEEEEEEEEEE 💅</ul>
+    </nav>
+  )
+}
+
+export default Navigation;
