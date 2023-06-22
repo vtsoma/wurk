9547afe42c61989817e6fefeb4e01e963ec8f68a
vtsoma <cheesetkh39@gmail.com>
2023-06-21 18:24:12 -0700

    Design development

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from "./components/nav";
 import './App.css'
 
 import userIcon from './assets/icons/user.svg';
+import colorPath from './assets/images/color-path.svg';
 
 function App() {
   return (
@@ -17,7 +18,13 @@ function App() {
       </div>
     </div>
     <main>
-      <h1 className={"main-text"}>Get your wurk dun by <br/> your choice</h1>
+      <article>
+        <h1 className={"main-text"}>Get your wurk dun by <br/> your choice</h1>
+        <p className={"desc-text"}>Our users can do anything</p>
+        <p className="desc-text">From delivery to murder</p>
+        <p className="desc-text">Sign up today!</p>
+        <img src={colorPath} alt="Color path cool"/>
+      </article>
     </main>
   </div>
   );
